86e13415b22d37ceb9e1f06327e3e51b1e5517e1
leoproject <leonardo2013ufs@gmail.com.br>
2022-10-18 23:44:24 +0100

    modified createFolder.py

--- createFolder.py
+++ createFolder.py
@@ -8,13 +8,15 @@ documents_dir =  os.path.join(root_dir,'documents/')
 others_dir = os.path.join(root_dir,'others/')
 software_dir = os.path.join(root_dir,'softwares/')
 ebooks_dir = os.path.join(root_dir,'ebooks/')
+log_dir = os.path.join(root_dir,'Log/')
 
 
-paths = [image_dir, documents_dir, others_dir, software_dir, ebooks_dir, ebooks_dir]
+paths = [image_dir, documents_dir, others_dir, software_dir, ebooks_dir,log_dir]
 
 for path in paths:
     isExist = os.path.exists(path)
     if not(isExist):
+        folder = path.split('/')[-2]
         os.mkdir(path)
     else:
         pass
